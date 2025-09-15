d7fb152c091d60cc0a94632dc839d8d86b324b99
dylannhardinn <dylan.m.hardin@gmail.com>
2025-09-15 18:07:23 +0000

    CompletedALien

---
+++ Assignment1/Project1.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;  
+
+int main (){
+cout <<         "||" "\n";
+cout <<   "______||"  "\n";
+cout <<  "/ ____ o|" "\n";
+cout << "| / ;; \\|"  "\n";
+cout << "| ______ |"   "\n";
+cout << "||______||"    "\n";
+cout << "||  D2  ||"  "\n";
+cout << "||______||"  "\n";
+cout << "|'\[--]/'|"  "\n";
+cout << "|  ¨''¨  |"  "\n";
+cout << "|  ''''  |"  "\n";
+cout << "|        |"  "\n";
+cout << "|        |"  "\n";
+cout << "|        |"  "\n";
+cout << "|________|"  "\n"; 
+}
